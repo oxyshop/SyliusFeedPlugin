acfcc04e2618a6596e629c656ed13a35f4676c83
Vojtěch Matějíček <vojtech.matejicek@oxyshop.cz>
2024-01-29 15:57:08 +0100

    fix: flash bag dependency

--- src/Controller/Action/Admin/ProcessFeedAction.php
+++ src/Controller/Action/Admin/ProcessFeedAction.php
@@ -6,38 +6,29 @@ namespace Setono\SyliusFeedPlugin\Controller\Action\Admin;
 
 use Setono\SyliusFeedPlugin\Message\Command\ProcessFeed;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class ProcessFeedAction
 {
-    private MessageBusInterface $commandBus;
-
-    private UrlGeneratorInterface $urlGenerator;
-
-    private FlashBagInterface $flashBag;
-
-    private TranslatorInterface $translator;
-
     public function __construct(
-        MessageBusInterface $commandBus,
-        UrlGeneratorInterface $urlGenerator,
-        FlashBagInterface $flashBag,
-        TranslatorInterface $translator
+        protected MessageBusInterface $commandBus,
+        protected UrlGeneratorInterface $urlGenerator,
+        protected RequestStack $requestStack,
+        protected TranslatorInterface $translator
     ) {
-        $this->commandBus = $commandBus;
-        $this->urlGenerator = $urlGenerator;
-        $this->flashBag = $flashBag;
-        $this->translator = $translator;
     }
 
     public function __invoke(int $id): RedirectResponse
     {
         $this->commandBus->dispatch(new ProcessFeed($id));
 
-        $this->flashBag->add('success', $this->translator->trans('setono_sylius_feed.feed_generation_triggered'));
+        $this->requestStack->getSession()->getBag('flashes')->add(
+            'success',
+            $this->translator->trans('setono_sylius_feed.feed_generation_triggered'),
+        );
 
         return new RedirectResponse($this->urlGenerator->generate('setono_sylius_feed_admin_feed_show', ['id' => $id]));
     }
